61445b5e2b748636a2a333c8b66d27b81a1edd2d
Juan Sierra <Juan Guillermo Sierra>
2020-10-19 11:22:19 -0500

    user story getRates working

--- .aws-sam/build/getRates/src/handlers/teachers/get-rates.js
+++ .aws-sam/build/getRates/src/handlers/teachers/get-rates.js
@@ -10,7 +10,7 @@ const docClient = new dynamodb.DocumentClient();
 /**
  * A simple example includes a HTTP get method to get all items from a DynamoDB table.
  */
-exports.getEvaluacionDocenteHandler = async (event) => {
+exports.getRatesHandler = async (event) => {
     if (event.httpMethod !== 'GET') {
         throw new Error(`getAllItems only accept GET method, you tried: ${event.httpMethod}`);
     }
