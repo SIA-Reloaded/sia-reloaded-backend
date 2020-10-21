f29a5910b6bbe7c4f84cbc3e2510c6d1405cdb7a
Alejandro Díaz Vecchio <alejandrod@keoworld.com>
2020-10-21 15:33:52 -0500

    added cors utils2

--- .aws-sam/build/getCourses/src/handlers/teachers/get-course.js
+++ .aws-sam/build/getCourses/src/handlers/teachers/get-course.js
@@ -4,32 +4,28 @@ const tableName = process.env.TABLE;
 const dynamodb = require('aws-sdk/clients/dynamodb');
 const docClient = new dynamodb.DocumentClient();
 
+const Utils = require('../../utils');
+
 exports.getCourseHandler = async (event) => {
-    if (event.httpMethod !== 'GET') {
-        throw new Error(`getAllItems only accept GET method, you tried: ${event.httpMethod}`);
+  if (event.httpMethod !== 'GET') {
+    throw new Error(`getAllItems only accept GET method, you tried: ${event.httpMethod}`);
+  }
+
+  const queryParameters = event.queryStringParameters
+
+  const params = {
+    TableName: tableName,
+    Key: {
+      "teacherID": queryParameters.teacherID,
+      "id": queryParameters.courseID
     }
+  };
 
-    console.info('received:', event);
-    const courseID = event.pathParameters.courseID;
-    const queryParameters = event.queryStringParameters
-    console.info(queryParameters, courseID)
-    const params = {
-        TableName: tableName,
-        Key: {
-            "teacherID": queryParameters.teacherID,
-            "id": courseID
-        }
-    };
-    console.info(params)
-    const data = await docClient.get(params).promise();
-    console.info(data)
-    const item = data.Item;
+  const data = await docClient.get(params).promise();
+  const item = data.Item;
 
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(item)
-    };
+  const response = Utils.prepareResponse(item);
 
-        console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
-    return response;
+  console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+  return response;
 }
--- .aws-sam/build/getCourses/src/handlers/teachers/get-courses.js
+++ .aws-sam/build/getCourses/src/handlers/teachers/get-courses.js
@@ -2,39 +2,38 @@ const tableName = process.env.TABLE;
 const dynamodb = require('aws-sdk/clients/dynamodb');
 const docClient = new dynamodb.DocumentClient();
 
+const Utils = require('../../utils');
+
+
+// Returns a list of curses for a given teacher, if a courseID is provided, returns that spectific course
 exports.getCoursesHandler = async (event) => {
-    if (event.httpMethod !== 'GET') {
-        throw new Error(`getcourses only accept GET method, you tried: ${event.httpMethod}`);
+  if (event.httpMethod !== 'GET') {
+    throw new Error(`getcourses only accept GET method, you tried: ${event.httpMethod}`);
+  }
+
+  const queryParameters = event.queryStringParameters;
+
+  const body = [];
+
+  const params = {
+    TableName: tableName,
+    FilterExpression: "#tchr = :tchrID",
+    ExpressionAttributeNames: {
+      "#tchr": "teacherID"
+    },
+    ExpressionAttributeValues: {
+      ":tchrID": queryParameters.teacherID
     }
+  };
 
-    let response = {
-        statusCode: 200,
-    };
+  const data = await docClient.scan(params).promise();
 
-    const body = [];
+  data.Items.forEach(function (item) {
+    body.push(item);
+  });
 
-    const queryParameters = event.queryStringParameters    
-    console.log(queryParameters);
-    const params = {
-        TableName: tableName,
-        FilterExpression: "#tchr = :tchrID",
-        ExpressionAttributeNames:{
-            "#tchr": "teacherID"
-        },
-        ExpressionAttributeValues: {
-            ":tchrID": queryParameters.teacherID
-        }
-    };
-    const data = await docClient.scan(params).promise();
-    
-    console.log("Query succeeded.");
-    data.Items.forEach(function(item) {
-        body.push(item);
-    });
+  const response = Utils.prepareResponse(body);
 
-    response.body = JSON.stringify(body);
-
-    
-    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
-    return response;
+  console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+  return response;
 }
