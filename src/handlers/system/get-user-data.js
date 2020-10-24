30df1154080d4e1cf0cc106cbd25af7d8734276e
Alejandro Díaz Vecchio <alejandrod@keoworld.com>
2020-10-24 12:15:20 -0500

    added cors utility, added API yml file, fix some endopoints

--- src/handlers/system/get-user-data.js
+++ src/handlers/system/get-user-data.js
@@ -1,4 +1,3 @@
-
 const tableName = process.env.TABLE;
 
 const dynamodb = require('aws-sdk/clients/dynamodb');
@@ -6,26 +5,28 @@ const docClient = new dynamodb.DocumentClient();
 
 const Utils = require('../../utils');
 
-exports.getCourseHandler = async (event) => {
+exports.getByUserDataHandler = async (event) => {
   if (event.httpMethod !== 'GET') {
-    throw new Error(`getAllItems only accept GET method, you tried: ${event.httpMethod}`);
+    throw new Error(`getMethod only accept GET method, you tried: ${event.httpMethod}`);
   }
+  
+  const username = event.pathParameters.username;
+ 
 
-  const queryParameters = event.queryStringParameters
-
-  const params = {
+  var params = {
     TableName: tableName,
     Key: {
-      "teacherID": queryParameters.teacherID,
-      "id": queryParameters.courseID
-    }
+      "id": username
+    },
   };
+  console.log(params)
 
   const data = await docClient.get(params).promise();
   const item = data.Item;
-
+ 
   const response = Utils.prepareResponse(item);
 
+  // All log statements are written to CloudWatch
   console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
   return response;
 }
