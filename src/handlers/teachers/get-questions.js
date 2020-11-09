699dd6f3b8f33166a676310c445e883876e39a83
Juan Sierra <Juan Guillermo Sierra>
2020-11-09 02:50:49 -0500

    first commit

---
+++ src/handlers/teachers/get-questions.js
@@ -0,0 +1,27 @@
+const tableName = process.env.TABLE;
+
+const dynamodb = require('aws-sdk/clients/dynamodb');
+const docClient = new dynamodb.DocumentClient();
+
+const Utils = require('../../utils');
+
+exports.getQuestionsHandler = async (event) => {
+    if (event.httpMethod !== 'GET') {
+        throw new Error(`getAllItems only accept GET method, you tried: ${event.httpMethod}`);
+    }
+
+    const queryParameters = event.queryStringParameters
+
+    const params = {
+        TableName: tableName
+    };
+
+    const data = await docClient.scan(params).promise();
+    const item = data.Item;
+
+    const response = Utils.prepareResponse(Item);
+
+    // All log statements are written to CloudWatch
+    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+    return response;
+}
