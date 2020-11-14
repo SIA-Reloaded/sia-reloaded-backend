cdee281cc3408793964c4f081a6150723873594d
Juan Sierra <Juan Guillermo Sierra>
2020-11-14 11:24:23 -0500

    merging for parallel work

--- src/handlers/teachers/get-rates.js
+++ src/handlers/teachers/get-rates.js
@@ -1,56 +1,67 @@
-const tableName = process.env.TABLE;
+const tableNameTeacher = process.env.TABLE_TEACHER;
+const tableNameAcademicCalendar = process.env.TABLE_ACADEMIC_CALENDAR;
 
 const dynamodb = require("aws-sdk/clients/dynamodb");
 const docClient = new dynamodb.DocumentClient();
 
+const currentAcademicCalendar = require('./../system/get-current-academic-calendar')
+
 const Utils = require("../../utils");
 
 exports.getRatesHandler = async (event) => {
-  if (event.httpMethod !== "GET") {
-    throw new Error(
-      `getAllItems only accept GET method, you tried: ${event.httpMethod}`
+    if (event.httpMethod !== "GET") {
+        throw new Error(
+            `getAllItems only accept GET method, you tried: ${event.httpMethod}`
+        );
+    }
+
+    const { teacherID, academicCalendar } = event.queryStringParameters;
+
+    let params = [];
+
+
+    if (academicCalendar) {
+
+        for (i = 0; i < academicCalendar.length; i++) {
+            params = {
+                TableName: tableNameTeacher,
+                FilterExpression: "teacherID = :tID and academicCalendar = :idAcademicCalendar",
+                ExpressionAttributeValues: {
+                    ":tID": teacherID,
+                    ":idAcademicCalendar": academicCalendar[i],
+                }
+            }
+
+            
+        }
+
+    } else {
+
+        //KeyConditionExpression: "#yr = :yyyy and title between :letter1 and :letter2",
+        const academicCalendar = await currentAcademicCalendar.getCurrentAcademicCalendar(tableNameAcademicCalendar);
+        const idAcademicCalendar = academicCalendar.id;
+
+        params = {
+            TableName: tableNameTeacher,
+            FilterExpression:
+                "teacherID = :teacherID and academicCalendar  = :idAcademicCalendar",
+            ExpressionAttributeValues: {
+                ":teacherID": teacherID,
+                ":idAcademicCalendar": idAcademicCalendar
+            },
+        };
+    }
+
+    const data = await docClient.scan(params).promise();
+    const item = data.Items;
+
+    console.info("data: ", data);
+
+    const response = Utils.prepareResponse(item);
+
+    // All log statements are written to CloudWatch
+    console.info(
+        `response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`
     );
-  }
-
-  const { teacherID, academicCalendar } = event.queryStringParameters;
-
-  let params = {};
-  const academicCalendarInit = "201501";
-  const academicCalendarLast = "202002";
-
-  if (academicCalendar) {
-    params = {
-      TableName: tableName,
-      FilterExpression: "teacherID = :tID and academicCalendar = :aCalendar",
-      ExpressionAttributeValues: {
-        ":tID": teacherID,
-        ":aCalendar": academicCalendar,
-      },
-    };
-  } else {
-    //KeyConditionExpression: "#yr = :yyyy and title between :letter1 and :letter2",
-    params = {
-      TableName: tableName,
-      FilterExpression:
-        "teacherID = :tID and academicCalendar between :init and :last",
-      ExpressionAttributeValues: {
-        ":tID": teacherID,
-        ":init": academicCalendarInit,
-        ":last": academicCalendarLast,
-      },
-    };
-  }
-
-  const data = await docClient.scan(params).promise();
-  const item = data.Items;
-
-  console.info("data: ", data);
-
-  const response = Utils.prepareResponse(item);
-
-  // All log statements are written to CloudWatch
-  console.info(
-    `response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`
-  );
-  return response;
+    return response;
 };
