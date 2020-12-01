66d5f8b706df8898eef81ac9fad4e1e5aef88ea2
Juan Sierra <Juan Guillermo Sierra>
2020-12-01 02:29:54 -0500

    added get course by code lambda to fetch all group of courses being a student/teacher

---
+++ src/handlers/teachers/get-course-by-code.js
@@ -0,0 +1,47 @@
+const tableName = process.env.TABLE;
+
+const dynamodb = require('aws-sdk/clients/dynamodb');
+const docClient = new dynamodb.DocumentClient();
+
+const Utils = require('../../utils');
+
+exports.getCourseByCodeHandler = async (event) => {
+  if (event.httpMethod !== 'GET') {
+    throw new Error(`getMethod only accept GET method, you tried: ${event.httpMethod}`);
+  }
+
+  console.log(event)
+
+  const {courseCode, groupID } = event.queryStringParameters
+  
+  let params;
+  if (groupID) {
+    params = {
+      TableName: tableName,
+      FilterExpression: 'code = :c and id = :i',
+      ExpressionAttributeValues: {
+        ':c': courseCode,
+        ':i': groupID,
+      }
+    };
+  } else {
+    params = {
+      TableName: tableName,
+      FilterExpression: 'code = :c',
+      ExpressionAttributeValues: {
+        ':c': courseCode,
+      }
+    };
+  }
+  
+  const data = await docClient.scan(params).promise()
+  const item = data.Items;
+
+  console.info('data: ', data)
+ 
+  const response = Utils.prepareResponse(item);
+
+  // All log statements are written to CloudWatch
+  console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+  return response;
+}
